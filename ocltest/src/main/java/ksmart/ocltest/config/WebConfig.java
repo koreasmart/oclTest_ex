84912137cb6ccefa55cd4be8d3774f3b3621fb0d
plmasterhong <coolhongs@gmail.com>
2022-09-26 13:00:14 +0900

    로그 설정

--- ocltest/src/main/java/ksmart/ocltest/config/WebConfig.java
+++ ocltest/src/main/java/ksmart/ocltest/config/WebConfig.java
@@ -1,13 +1,33 @@
 package ksmart.ocltest.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import ksmart.ocltest.interceptor.CommonInterceptor;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	private final CommonInterceptor commonInterceptor;
+	
+	public WebConfig(CommonInterceptor commonInterceptor) {
+		this.commonInterceptor = commonInterceptor;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(commonInterceptor)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/css/**")
+				.excludePathPatterns("/images/**")
+				.excludePathPatterns("/js/**")
+				.excludePathPatterns("/favicon.ico");
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**")
